062b5f377ec5d14119a0bfa2b9a6c6e884366afc
Pratik Kubal <ubuntu@ip-172-31-29-43.us-east-2.compute.internal>
2019-05-07 22:25:56 +0000

    Changing Keras Code

--- helper.py
+++ helper.py
@@ -11,7 +11,6 @@ class DataLoader():
         self.img_res = img_res
         self.batch_size = batch_size
         self.path = glob(path+'/%s/*' % (self.dataset_name))
-        print(self.path)
 
     def load_data(self, is_testing=False):
 
@@ -43,3 +42,35 @@ class DataLoader():
 
     def imread(self, path):
         return imageio.imread(path).astype(np.float)
+    
+def sample_images(gen, epoch, dataset_name,imgs_hr, imgs_lr):
+        os.makedirs('images/%s' % dataset_name, exist_ok=True)
+        r, c = 2, 2
+
+        fake_hr = gen.predict(imgs_lr)
+
+        # Rescale images 0 - 1
+        imgs_lr = 0.5 * imgs_lr + 0.5
+        fake_hr = 0.5 * fake_hr + 0.5
+        imgs_hr = 0.5 * imgs_hr + 0.5
+
+        # Save generated images and the high resolution originals
+        titles = ['Generated', 'Original']
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for row in range(r):
+            for col, image in enumerate([fake_hr, imgs_hr]):
+                axs[row, col].imshow(image[row])
+                axs[row, col].set_title(titles[col])
+                axs[row, col].axis('off')
+            cnt += 1
+        fig.savefig("images/%s/%d.png" % (dataset_name, epoch))
+        plt.close()
+
+        # Save low resolution images for comparison
+        for i in range(r):
+            fig = plt.figure()
+            plt.imshow(imgs_lr[i])
+            fig.savefig('images/%s/%d_lowres%d.png' %
+                        (dataset_name, epoch, i))
+            plt.close()
